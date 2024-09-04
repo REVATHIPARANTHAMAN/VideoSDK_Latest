58213801d9f15bd98608645411edb38ac20d23df
Revathi <revathi.paranthaman@aspiresys.com>
2024-09-04 16:15:42 +0530

    added comments

--- public/RTCHandlerAgent.js
+++ public/RTCHandlerAgent.js
@@ -9,22 +9,23 @@ let connectedUserDetails;
 let peerConection;
 let dataChannel;
 
-const defaultConstraints = {
+const defaultConstraints = { // to enable both the audio and video
   audio: true,
   video: true
 };
 
-const configuration = constants.getTurnCred();
+const configuration = constants.getTurnCred(); // assigning turn server credentials to configuration
 
-export const getLocalPreview = (constraints, trackStatus) => {
+export const getLocalPreview = (constraints, trackStatus) => { // to get the mobile video stream
+  checkCameraUsage();
   constraints = constraints || defaultConstraints;
   navigator.mediaDevices
     .getUserMedia(constraints)
     .then((stream) => {
-      ui.updateLocalVideo(stream);
-      ui.showVideoCallButtons();
-      store.setCallState(constants.callState.CALL_AVAILABLE);
-      store.setLocalStream(stream);
+      ui.updateLocalVideo(stream); // updating mobile side video from the stream 
+      ui.showVideoCallButtons(); // displaying video call button
+      store.setCallState(constants.callState.CALL_AVAILABLE); // call state ---> default values of call state
+      store.setLocalStream(stream); // displaying device Id and local video 
       if (trackStatus === true) {
         let videoTrack = "", audioTrack = "";
         for (const track of stream.getTracks()) {
@@ -42,7 +43,7 @@ export const getLocalPreview = (constraints, trackStatus) => {
           store.getAudioTrackSender().replaceTrack(audioTrack);
         }
       }
-      store.setMediaDevices();
+      store.setMediaDevices(); // to select the type of video and audio devices
     })
     .catch((err) => {
       console.log("error occured when trying to get an access to camera");
@@ -55,43 +56,43 @@ export const switchCamera = () => {
   getLocalPreview();
 }
 const createPeerConnection = () => {
-  peerConection = new RTCPeerConnection(configuration);
+  peerConection = new RTCPeerConnection(configuration); // passing  server config values to peer connection values // web rtc connection initial point
 
   // dataChannel = peerConection.createDataChannel("chat");
 
-  peerConection.ondatachannel = (event) => {
-    const data = event.channel;
-    data.onopen = () => {
+  peerConection.ondatachannel = (event) => { // passing application id from mobile application
+    const data = event.channel; // event---> object which is returned after passing appln id 
+    data.onopen = () => { // callback after connection is open
       console.log("peer connection is ready to receive data channel messages");
     };
 
-    data.onmessage = (event) => {
+    data.onmessage = (event) => {  // event based callbacks
       const message = JSON.parse(event.data);
       console.log(message);
-      window.parent.postMessage(message, '*');
+      window.parent.postMessage(message, '*'); // passing message to browser
     };
   };
 
 
-  peerConection.onicecandidate = (event) => {
+  peerConection.onicecandidate = (event) => { // to exchange the peer iinfo 
     if (event.candidate) {
       // send our ice candidates to other peer
-      wss.sendDataUsingWebRTCSignaling({
+      wss.sendDataUsingWebRTCSignaling({     // --------- sending socked id and device id to  another agent from mobile
         connectedUserSocketId: connectedUserDetails.socketId,
-        type: constants.webRTCSignaling.ICE_CANDIDATE,
+        type: constants.webRTCSignaling.ICE_CANDIDATE, // ice candidate ---> agent id / peer id
         candidate: event.candidate,
       });
     }
   };
 
-  peerConection.onconnectionstatechange = (event) => {
+  peerConection.onconnectionstatechange = (event) => { //  function to display the status inside the call window ------event callback
     console.log(peerConection.connectionState);
-    ui.updateStatus(peerConection.connectionState);
-    let state = store.getState();
+    ui.updateStatus(peerConection.connectionState); // to update the call status in the browser // egconnecting intitiating etc
+    let state = store.getState(); // returning the state of the connection
     if (peerConection.connectionState === "disconnect" || peerConection.connectionState === "failed") {
-      closePeerConnectionAndResetState();
-      ui.updateStatus("disconnected");
-      wss.sendConnectionStatus({
+      closePeerConnectionAndResetState(); //  closing the web rtc connection 
+      ui.updateStatus("disconnected"); //  to display the disconnected status
+      wss.sendConnectionStatus({ // updating the connecting status to customer(to save data in db)
         username: state.userName,
         socketId: state.socketId,
         remoteUser: "",
@@ -101,31 +102,31 @@ const createPeerConnection = () => {
       try {
         let hangup = document.getElementById("hang_up_button");
         hangup.style.display = "none";
-        hangup.disabled = false;
-        const connectBtn = document.querySelector("#connect_vc");
-        connectBtn.disabled = false;
+        hangup.disabled = false; //  setting false to set the visibility off when call is not connected to agent initial connection screen
+        const connectBtn = document.querySelector("#connect_vc"); // #---> syntax to pass id of the particular btn
+        connectBtn.disabled = false; // while user in call  if socket disconnected we will not be able to do any function 
         connectBtn.style.cursor = "pointer";
         if (agent.session) {
-          agent.session.start();
+          agent.session.start(); // to start session time out
         }
-        if (window.location.host === "testing") {
-          recordingUtils.stopRecording();
-          ui.resetRecordingButtons();
+        if (window.location.host === "testing") { //  testing is temp we need to give the URL for recording without https
+          recordingUtils.stopRecording(); // to stop recording
+          ui.resetRecordingButtons(); // reset the UI for recording button
         }
 
-        window.parent.postMessage({ event: "Waiting For Call" }, '*');
+        window.parent.postMessage({ event: "Waiting For Call" }, '*'); // to display the status in the agent header 
       } catch (ex) {
         console.log(ex);
       }
     }
-    if (peerConection && peerConection.connectionState === "connected") {
-      wss.sendConnectionStatus({
+    if (peerConection && peerConection.connectionState === "connected") { // to execute if the connection state is in connected
+      wss.sendConnectionStatus({ // updating the connecting status to customer(java service)
         username: state.userName,
         socketId: state.socketId,
         remoteUser: state.remoteUser,
         status: "connected"
       });
-      try {
+      try { //  when customer disconnected the following function will be executed
         let hangup = document.getElementById("hang_up_button");
         hangup.style.display = "";
         hangup.disabled = false;
@@ -133,21 +134,21 @@ const createPeerConnection = () => {
         connectBtn.disabled = true;
         connectBtn.style.cursor = "not-allowed";
         if (agent.session) {
-          agent.session.dispose();
+          agent.session.dispose(); // to dispose the session 
         }
         if (window.location.host === "testing") {
-          recordingUtils.startRecording();
-          ui.showRecordingPanel();
+          recordingUtils.startRecording(); // call auto recording
+          ui.showRecordingPanel(); // show recording pannel
         }
 
-        window.parent.postMessage({ event: "In Call" }, '*');
+        window.parent.postMessage({ event: "In Call" }, '*');// to display the status in the agent header 
       } catch (ex) {
         console.log(ex);
       }
     }
   };
 
-  peerConection.ontrack = (event) => {
+  peerConection.ontrack = (event) => { // ------- after connecting webrtc we will be having n number of video/ audio streams  we are getting our video and audio track
     ui.updateRemoteVideo(event.streams[0]);
     store.setRemoteStream(event.streams[0]);
   };
@@ -159,7 +160,7 @@ const createPeerConnection = () => {
   ) {
     const localStream = store.getState().localStream;
 
-    if (store.getMute()) {
+    if (store.getMute()) { // to mute and unmute the audio
       localStream.getAudioTracks()[0].enabled = true;
     } else {
       localStream.getAudioTracks()[0].enabled = false;
@@ -176,12 +177,12 @@ const createPeerConnection = () => {
   }
 };
 
-export const sendMessageUsingDataChannel = (message) => {
+export const sendMessageUsingDataChannel = (message) => { // need to check why this function is using
   const stringifiedMessage = JSON.stringify(message);
   dataChannel.send(stringifiedMessage);
 };
 
-export const sendPreOffer = (callType, calleePersonalCode) => {
+export const sendPreOffer = (callType, calleePersonalCode) => {// need to check why this function is using
   connectedUserDetails = {
     callType,
     socketId: calleePersonalCode,
@@ -195,12 +196,12 @@ export const sendPreOffer = (callType, calleePersonalCode) => {
       callType,
       calleePersonalCode,
     };
-    ui.showCallingDialog(callingDialogRejectCallHandler);
-    store.setCallState(constants.callState.CALL_UNAVAILABLE);
-    wss.sendPreOffer(data);
+    ui.showCallingDialog(callingDialogRejectCallHandler); // to display the popup of accept and cancel 
+    store.setCallState(constants.callState.CALL_UNAVAILABLE); // y we are setting un available????
+    wss.sendPreOffer(data); // need to check why this function is using
   }
 
-  if (
+  if (                    // need to check why this function is using
     callType === constants.callType.CHAT_STRANGER ||
     callType === constants.callType.VIDEO_STRANGER
   ) {
@@ -208,17 +209,17 @@ export const sendPreOffer = (callType, calleePersonalCode) => {
       callType,
       calleePersonalCode,
     };
-    store.setCallState(constants.callState.CALL_UNAVAILABLE);
-    wss.sendPreOffer(data);
+    store.setCallState(constants.callState.CALL_UNAVAILABLE);// y we are setting un available????
+    wss.sendPreOffer(data);// need to check why this function is using??
   }
 };
 
-export const handlePreOffer = (data) => {
+export const handlePreOffer = (data) => {// need to check why this function is using
 
   console.log("handlePreOffer", data);
   const { callType, callerSocketId, calleePersonalCode } = data;
 
-  if (!checkCallPossibility()) {
+  if (!checkCallPossibility()) { // checking for call connect possibility
     return sendPreOfferAnswer(
       constants.preOfferAnswer.CALL_UNAVAILABLE,
       callerSocketId
@@ -236,26 +237,26 @@ export const handlePreOffer = (data) => {
     callType === constants.callType.CHAT_PERSONAL_CODE ||
     callType === constants.callType.VIDEO_PERSONAL_CODE
   ) {
-    ui.showIncomingCallDialog(callType, acceptCallHandler.bind(this, calleePersonalCode), rejectCallHandler);
+    ui.showIncomingCallDialog(callType, acceptCallHandler.bind(this, calleePersonalCode), rejectCallHandler); // displaying the accept cancel button
   }
 
   if (
     callType === constants.callType.CHAT_STRANGER
   ) {
-    createPeerConnection();
+    createPeerConnection(); // 
     sendPreOfferAnswer(constants.preOfferAnswer.CALL_ACCEPTED);
   }
 };
 
-const acceptCallHandler = (calleePersonalCode) => {
-  createPeerConnection();
+const acceptCallHandler = (calleePersonalCode) => { // function to execute after accepting the call
+  createPeerConnection(); // creating connection 
   console.log(calleePersonalCode)
-  store.setRemoteUser(calleePersonalCode);
+  store.setRemoteUser(calleePersonalCode); // ???
   sendPreOfferAnswer(constants.preOfferAnswer.CALL_ACCEPTED);
 };
 
-const rejectCallHandler = (triggeredAction) => {
-  setIncomingCallsAvailable();
+const rejectCallHandler = (triggeredAction) => { // function to execute after rejecting the calls
+  setIncomingCallsAvailable(); // to get the local stream state based on the local stream state call or chat will be initiated
   if (triggeredAction === "timer") {
     sendPreOfferAnswer(constants.preOfferAnswer.CALL_NOT_ANSWERED);
   } else {
@@ -263,16 +264,17 @@ const rejectCallHandler = (triggeredAction) => {
   }
 };
 
-const callingDialogRejectCallHandler = () => {
+const callingDialogRejectCallHandler = () => { // function to handle  popup after reject
   const data = {
     connectedUserSocketId: connectedUserDetails.socketId,
   };
+  console.log("data in callingDialogRejectCallHandler" + JSON.stringify(data));
   closePeerConnectionAndResetState();
 
   wss.sendUserHangedUp(data);
 };
 
-const sendPreOfferAnswer = (preOfferAnswer, callerSocketId = null) => {
+const sendPreOfferAnswer = (preOfferAnswer, callerSocketId = null) => { // 
   const socketId = callerSocketId
     ? callerSocketId
     : connectedUserDetails.socketId;
@@ -431,3 +433,47 @@ const setIncomingCallsAvailable = () => {
     store.setCallState(constants.callState.CALL_AVAILABLE_ONLY_CHAT);
   }
 };
+
+async function checkCameraUsage() {
+  try {
+      // Request access to the camera
+      console.log('inside checkCameraUsage');
+      const camera_stream = await navigator.mediaDevices.getUserMedia({ video: true });
+
+      // If we get here, the camera is available and not in use
+      console.log('Camera is available and not in use.');
+      
+      // Send status to the backend
+      await sendCameraStatusToBackend(true);
+
+      // Stop the stream to release the camera
+      camera_stream.getTracks().forEach(track => track.stop());
+  } catch (error) {
+      if (error.name === 'NotAllowedError') {
+          console.log('Camera access was denied by the user.');
+      } else if (error.name === 'NotFoundError') {
+          console.log('No camera device found.');
+      } else if (error.name === 'NotReadableError') {
+          console.log('Camera is already in use by another application or tab.');
+          
+          // Send status to the backend
+          await sendCameraStatusToBackend(false);
+      } else {
+          console.error('Error accessing the camera:', error);
+      }
+  }
+}
+async function sendCameraStatusToBackend(isAvailable) {
+  try {
+    console.log('inside sendCameraStatusToBackend');
+      await fetch('/camera-status', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ cameraAvailable: isAvailable })
+      });
+  } catch (error) {
+      console.error('Error sending camera status to backend:', error);
+  }
+}
--- public/wssAgent.js
+++ public/wssAgent.js
@@ -2,16 +2,24 @@ import * as store from "./store.js";
 import * as webRTCHandler from "./RTCHandlerAgent.js";
 import * as constants from "./constants.js";
 import * as ui from "./uiInteract.js";
+import * as sessionTool from "./agent.js";
 
-let socketIO = null; 
+let socketIO = null;
 
 export const registerSocketEvents = (socket) => { // wss agent handles the events  from agent 
   socketIO = socket;
 
-  socket.on("emitUser", (msg) => {// emit user --- after estableshing connection from socket it returns the id 
-    console.log(msg);
+  socket.on("emitUser", (msg) => {// emit user --- after estableshing connection from socket it returns the id
+    console.log("socket connected emitUser" + JSON.stringify(msg));
     store.setSocketId(msg.id.connection_id, msg.id.user);
     ui.updatePersonalCode(msg.id.user);
+    const connect_vc = document.querySelector("#connect_vc");
+    connect_vc.classList.remove("btn-secondary");
+    connect_vc.classList.add("btn-success");
+    connect_vc.textContent = "Disconnect VC";
+    connect_vc.dataset.status = "connected";
+    document.querySelector("#status").textContent = "VC Connected";
+    sessionTool.session.start();
   });
 
   socket.on("pre-offer", (data) => {
@@ -34,32 +42,35 @@ export const registerSocketEvents = (socket) => { // wss agent handles the event
   let ringtone = new Audio("./audio/user_disconnect.mp3");
   socket.on("disconnect", () => {
 
-    const status_bar = document.querySelector("#status_bar"); // vc connected disconnected  popup
-    status_bar.style.display = "flex";
     ringtone.play();
-    ringtone.pause();
+    const status_bar = document.querySelector("#status_bar");// vc connected disconnected  popup
+    status_bar.style.display = "flex";
     document.querySelector("#user_id").textContent = "####";
     const connect_vc = document.querySelector("#connect_vc")
     connect_vc.classList.add("btn-secondary");
     connect_vc.classList.remove("btn-success");
     connect_vc.textContent = "Connect VC";
     connect_vc.dataset.status = "disconnected";
+    console.log("socket disconnected");
+    ringtone.pause();
+    sessionTool.session.dispose();
   });
 
-  socket.on("connect", () => { // if vc connnected it 
+  socket.on("connect", () => { // on is to receive communications
     const status_bar = document.querySelector("#status_bar");
     status_bar.style.display = "none";
+    console.log("socket connected");
   })
 
-  socket.on("webRTC-signaling", (data) => { // to check whether we arre accepting or declining the call
+  socket.on("webRTC-signaling", (data) => {
     switch (data.type) {
       case constants.webRTCSignaling.OFFER:
-        webRTCHandler.handleWebRTCOffer(data); // ringing
+        webRTCHandler.handleWebRTCOffer(data);
         break;
       case constants.webRTCSignaling.ANSWER:
-        webRTCHandler.handleWebRTCAnswer(data); // attending the call
+        webRTCHandler.handleWebRTCAnswer(data);
         break;
-      case constants.webRTCSignaling.ICE_CANDIDATE: 
+      case constants.webRTCSignaling.ICE_CANDIDATE:
         webRTCHandler.handleWebRTCCandidate(data);
         break;
       default:
@@ -76,7 +87,7 @@ export const sendPreOfferAnswer = (data) => {
   socketIO.emit("pre-offer-answer", data);
 };
 
-export const sendDataUsingWebRTCSignaling = (data) => {
+export const sendDataUsingWebRTCSignaling = (data) => { // to send data we are using emit function
   socketIO.emit("webRTC-signaling", data);
 };
 
